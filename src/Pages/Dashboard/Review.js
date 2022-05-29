b2bbe791859166be9d7ac16c66e7e1993caa6ae2
RiadArefin <arefinriad40@gmail.com>
2022-05-29 23:38:00 +0600

    avatard add

--- src/Pages/Dashboard/Review.js
+++ src/Pages/Dashboard/Review.js
@@ -5,6 +5,11 @@ const Review = ({review}) => {
         <div className="card  bg-base-100 shadow-xl">
        
         <div className="card-body items-center text-center">
+        <div className="avatar offline">
+  <div className="w-24 rounded-full">
+    <img src="https://api.lorem.space/image/face?hash=40361" />
+  </div>
+</div>
           <h2 className="card-title text-teal-500 text-2xl">{review?.name}</h2>
            <p>Review: {review?.rev}</p>
            <p className='text-orange-400'>Rating: {review?.ratings} out of 5</p>
