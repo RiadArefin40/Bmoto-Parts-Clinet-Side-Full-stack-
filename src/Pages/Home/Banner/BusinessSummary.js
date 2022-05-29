c21c944b134bed678e7c9359e933af2d58de6a9c
RiadArefin <arefinriad40@gmail.com>
2022-05-29 23:34:28 +0600

    readme not found add

--- src/Pages/Home/Banner/BusinessSummary.js
+++ src/Pages/Home/Banner/BusinessSummary.js
@@ -44,6 +44,9 @@ const BusinessSummary = () => {
 </div>
 <div className="card  bg-base-100 shadow-xl">
   <div className="card-body bg-teal-400">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 mx-auto text-yellow-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+</svg>
   <h2 className="text-3xl text-center font-bold">372+</h2>
   <p className='text-white'>Supply  over the world</p>
     <div className="card-actions justify-end">
